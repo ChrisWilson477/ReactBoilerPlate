5703d2239b1abee6d5b54ce261b1f3e0c0e2adb9
Chris <68999991+ChrisWilson477@users.noreply.github.com>
2021-03-21 12:07:29 -0600

    added css loaders to Boilerplate and updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,32 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/App.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'public')
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
-    }
-}
+	entry: './src/App.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.join(__dirname, 'public'),
+	},
+	module: {
+		rules: [
+			{
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(jpg|png)$/,
+				use: {
+					loader: 'url-loader',
+				},
+			}
+		],
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'public'),
+	},
+	mode: 'development',
+};
